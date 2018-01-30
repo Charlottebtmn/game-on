dbe31c9526a083d066522a2ed7d60ee093bac4bc
VivianSolide <vivian.sarazin@gmail.com>
2018-01-30 13:27:35 +0100

    models // user.js // git pr test

--- models/user.js
+++ models/user.js
@@ -9,6 +9,21 @@ const userSchema = new Schema({
   imgUrl: {
     type: String,
     default: "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250"
+  },
+  // games owned
+  _game: {
+    type: Schema.Types.ObjectId,
+    ref: 'Game'
+  },
+  // user belongs to X team 
+  _team: {
+    type: Schema.Types.ObjectId,
+    ref: 'Team'
+  },
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
   }
 });
 
