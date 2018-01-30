696d649a77a5799d9712a69917f160f6421b2ddb
VivianSolide <vivian.sarazin@gmail.com>
2018-01-30 13:36:01 +0100

    models // user.js // git pr test

---
+++ models/game.js
@@ -0,0 +1,32 @@
+// models/campaign.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const User = require('./user');
+// const Types = require('.type');
+
+const GameSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  // category: {
+  //   type: String,
+  //   enum: Types,
+  //   required: true
+  // },
+  _owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+
+module.exports = mongoose.model('Game', GameSchema);
