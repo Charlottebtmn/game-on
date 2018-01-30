f6e45f3488c66bd7b568f5726f22ffa55f784420
VivianSolide <vivian.sarazin@gmail.com>
2018-01-30 13:20:13 +0100

    models // user.js // git pr test

--- models/user.js
+++ models/user.js
@@ -1,1 +1,16 @@
-lorem
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: String,
+  username: String,
+  password: String,
+  description: String,
+  imgUrl: {
+    type: String,
+    default: "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250"
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
